c86c7dbf6c87962aaa64fa35bf6f47ccd374fb66
Manu Gupta <mgupta8143@gmail.com>
2025-03-13 22:47:41 -0400

    Add comment to binary

--- operations.py
+++ operations.py
@@ -4,15 +4,16 @@ from abc import ABC, abstractmethod
 An operation represents the simplest execution that can occur
 within an expression. Operations can operate across multiple
 characters in a language and can be represented in multiple ways.
-
 """
 class Operation(ABC):
     @abstractmethod
     def execute(self, *operands: list[str]) -> str:
         pass 
 
-    
-
+"""
+A binary operation represents the simplest operation, in that
+it can only operate on two operands.
+"""
 class BinaryOperation(Operation):
     def execute(self, *operands: list[str]) -> str:
         assert len(operands) == 2
