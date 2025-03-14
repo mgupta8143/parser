a2ed9bacbfed4551b65c401060bd42a2da38b39a
Manu Gupta <mgupta8143@gmail.com>
2025-03-13 22:45:19 -0400

    small setup

--- operations.py
+++ operations.py
@@ -1,3 +1,12 @@
-class Operation:
+from abc import ABC, abstractmethod
 
-    
+"""
+An operation represents the simplest execution that can occur
+within an expression. Operations can operate across multiple
+characters in a language and can be represented in multiple ways.
+
+"""
+class Operation(ABC):
+
+    @abstractmethod
+    def execute(self, *operands: list[str]) -> str:
