7977d18aa89fac93faa6e75d60fd6d2dfe20bbaa
Manu Gupta <mgupta8143@gmail.com>
2025-03-13 22:46:01 -0400

    binary operation

--- operations.py
+++ operations.py
@@ -7,6 +7,21 @@ characters in a language and can be represented in multiple ways.
 
 """
 class Operation(ABC):
-
     @abstractmethod
     def execute(self, *operands: list[str]) -> str:
+        pass 
+
+    
+
+class BinaryOperation(Operation):
+    def execute(self, *operands: list[str]) -> str:
+        assert len(operands) == 2
+        self.execute(operands[0], operands[1])
+    
+    @abstractmethod
+    def execute(self, operand1: str, operand2: str) -> str:
+        pass 
+
+class Addition(BinaryOperation):
+
+    def execute(self, operand1: str, operand2: str) -> str:
